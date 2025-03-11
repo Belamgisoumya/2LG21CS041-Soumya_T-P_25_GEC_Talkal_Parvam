5f4448d416cfc15cfee58d6eb160e5cb44daae8e
SOUMYA <belamgisoumya16@gmail.com>
2025-03-11 16:36:21 +0530

    new changes with bootstrap lib

--- 10 mar  25/student_mgmt/student_mgmt/student/views.py
+++ 10 mar  25/student_mgmt/student_mgmt/student/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def student_form(request):
+    if request.method =='POST':
+        print("Form data sname:", request.POST['sname'])
+        print("Form data name:", request.POST['name'])
     return render(request, 'students/student_form.html')
